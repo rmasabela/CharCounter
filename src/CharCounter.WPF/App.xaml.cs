325b668a37bf4c8b8035e65927a2cab6fb4e496b
Ricardo Masabel A. <r.masabel.a@hotmail.com>
2026-09-04 17:03:32 -0500

    test(wpf): exclude native UI infrastructure classes from code coverage
    Decorate App and WpfClipboardService with [ExcludeFromCodeCoverage] so metrics reflect presentation logic testability.

--- src/CharCounter.WPF/App.xaml.cs
+++ src/CharCounter.WPF/App.xaml.cs
@@ -1,16 +1,18 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using RMALabs.CharCounter.Core.Services;
 using RMALabs.CharCounter.WPF.Services;
 using RMALabs.CharCounter.WPF.ViewModels;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
 
 namespace RMALabs.CharCounterWPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public partial class App : Application
     {
         private ServiceProvider? _serviceProvider;
--- src/CharCounter.WPF/Services/WpfClipboardService.cs
+++ src/CharCounter.WPF/Services/WpfClipboardService.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 
 namespace RMALabs.CharCounter.WPF.Services
 {
+    [ExcludeFromCodeCoverage]
     public class WpfClipboardService : IClipboardService
     {
         public void SetText(string text)
